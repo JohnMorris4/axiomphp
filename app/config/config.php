22aa3e0474a68404061d490d524231df34150a6c
JohnMorris4 <johnmorris@morrisje.com>
2017-10-20 14:44:01 -0400

    Final prior to testing

--- app/config/config.php
+++ app/config/config.php
@@ -2,8 +2,8 @@
 //Define Database Params
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'jmvc');
+define('DB_PASS', 'root');
+define('DB_NAME', 'local_db');
 //app root
 define('APPROOT', dirname(dirname (__FILE__)));
 //Define the URL Root
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,10 +1,13 @@
 <?php
     class Pages extends Controller {
         public function __construct(){
-            
-        }
+          }
         public function index(){
-            $data = ['title' => 'Welcome'];
+            $data = [
+                'title' => 'Welcome'
+                
+            ];
+            
             $this->view('pages/index', $data);
         }
 
