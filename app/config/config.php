d0c33f7148dde0c73981db015cf9525fafc503c7
JohnMorris4 <johnmorris@morrisje.com>
2017-10-20 07:16:41 -0400

    Removden Initial Source Files and started getting the Main APP setup.

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+//app root
+define('APPROOT', dirname(dirname (__FILE__)));
+//Define the URL Root
+define('URLROOT', 'http://localhost/axiomphp');
+//Add Site Name
+define('SITENAME', 'AxiomPHP');
---
+++ app/views/pages/index.php
@@ -0,0 +1,2 @@
+<h1><?php echo $data['title']; ?></h1>
+
